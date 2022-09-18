510a4401330b90505aa49fddedc8768ed7c15c34
dougaler <dougaler@mail.uc.edu>
2022-09-18 13:35:35 -0400

    Completed Even or Odd program

---
+++ src/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int numToExamine = 400;
+        if((numToExamine % 2) == 0) {
+            System.out.println("The number " + numToExamine + " is Even");
+        }else{
+            System.out.println("The number " + numToExamine + " is Odd");
+        }
+
+
+    }
+}
